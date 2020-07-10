97301c11741bec2e07a9b92f460cb55314c9d463
yukonscared <68118002+yukonscared@users.noreply.github.com>
2020-07-10 17:24:38 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# Discord-Nitro-Gift-Brute-Force
+Brute Force Nitro Gift Link Program ,` im not the program creator .`
+```
+NEED PROXIES !
+```
+
+# How to Use ? :
+just dowload and open run.bat
+---------------------
---
+++ run.bat
@@ -0,0 +1,22 @@
+echo off
+color 05  
+echo starting 
+echo .
+timeout /t 1
+echo ..
+timeout /t 1
+echo ...
+
+echo presse any key for open the menu
+pause
+start DiscordGiftCodeBruteForcer.exe
+start proxychecker.exe 
+echo              /$$                           /$$                     /$$
+echo              $$                           $$                     $$
+echo   /$$$$$$$ /$$$$$$    /$$$$$$   /$$$$$$  /$$$$$$    /$$$$$$   /$$$$$$$
+echo  /$$_____/_  $$_/   ____  $$ /$$__  $$_  $$_/   /$$__  $$ /$$__  $$
+echo   $$$$$$    $$      /$$$$$$$ $$  \__/   $$     $$$$$$$$ $$   $$
+echo  \____  $$   $$ /$$ /$$__  $$ $$         $$ /$$ $$_____/ $$   $$
+echo  /$$$$$$$/    $$$$/  $$$$$$$ $$          $$$$/  $$$$$$$  $$$$$$$
+echo _______/    \___/   \_______/__/         \___/   \_______/ \_______/
+pause
